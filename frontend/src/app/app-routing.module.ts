de06edaac0819e8389568a39e4583b81e5a84061
Mark <mcac022803@gmail.com>
2025-07-16 11:55:53 +0800

    feat(sidebar): add css

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/patients',
+    pathMatch: 'full'
+  },
+  {
     path: 'patients',
     loadChildren: () =>
       import('./modules/patients/patients.module').then(m => m.PatientsModule)
--- frontend/src/app/modules/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/modules/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  sidebarHeight: string = '93vh';
+  isMobile: boolean = false;
+  isSidebarVisible: boolean = true;
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).subscribe(result => {
+      if (result.breakpoints[Breakpoints.XSmall] || result.breakpoints[Breakpoints.Small]) {
+        this.sidebarHeight = '100vh';
+        this.isMobile = true;
+        this.isSidebarVisible = false;
+      } else {
+        this.sidebarHeight = '93vh';
+        this.isMobile = false;
+        this.isSidebarVisible = true;
+      }
+    });
+  }
+
+  toggleSidebar(): void {
+    this.isSidebarVisible = !this.isSidebarVisible;
+  }
 }
